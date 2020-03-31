e643fa0197e7c67fd4413fd6c8e8205874986260
Felix Anducho <felix.anducho74@gmail.com>
2020-03-30 21:35:00 -0600

    Add 'Node.js' section

--- src/index.js
+++ src/index.js
@@ -1,57 +1,93 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const exercises = {
-    one: 10,
-    two: 7,
-    three: 14
+const TotalExercises = ({ exercises }) =>{
+    let initialValue = 0;
+    const total = exercises.reduce( (accumulator , currentValue) => {
+        return accumulator + currentValue.exercises
+    }, initialValue)
+    return(
+        <p><strong>Total of {total} exercises</strong></p>
+    ) 
 }
 
-const Header = (props) => {
+
+const Content = ({ id, course }) =>{
+    return <li key={id}>
+        {course.name} - {course.exercises} exercices
+            </li>
+}
+
+const Course = (props) => {
+    const {course} = props;
+    const parts = course.parts;
 
     return (
-    <h1>
-        {props.course}
-    </h1>
-    )
-}
-
-const Part = (props) =>{
-    
-    return(
-    <p>{props.part} {props.exercies}</p>
-    )
-}
-
-const Content = () =>{
-    const part1 = 'Fundamentals of React';
-    const part2 = 'Using props to pass data';
-    const part3 = 'State of a component';
-
-    return(
     <div>
-        <Part part={part1} exercies={exercises.one}/>
-        <Part part={part2} exercies={exercises.two}/>
-        <Part part={part3} exercies={exercises.three}/>
+        <h1>{course.name}</h1>
+        <ul>
+            {course.parts.map((part, id) =>
+            <Content key={id} course={part}/>
+            )}
+        </ul>
+        <TotalExercises exercises={parts}/>
     </div>
     )
 }
-
-const Total = () =>{
-
-    return (
-    <strong><p>Number of exercises { exercises.one + exercises.two + exercises.three}</p></strong>
- )
-}
-
-
+    
 const App = () => {
-    const course = 'Half stack application development';
+
+    const course = [
+        { 
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name:'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 3
+
+            }
+        ]
+    },
+    {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+            {
+                name: 'Routing',
+                exercises: 3,
+                id: 1 
+            },
+            {
+                name:'Middlewares',
+                exercises: 7,
+                id: 2
+            }
+        ]
+    }     
+ ]
+
+
   return (
     <div>
-        <Header course={course} />
-        <Content/>
-        <Total/>
+        <Course course={course[0]}/>
+        <Course course={course[1]}/>
     </div>
   )
 }
